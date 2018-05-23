69990272aaf76b25f30c7bccc7f9b5a18fe31b62
Romain Racamier-Lafon <romain.racamier@gmail.com>
2018-05-23 13:53:54 +0200

    reproduce write problem

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 console.log('issue : start')
 
-const { ExifTool } = require("exiftool-vendored")
+const {
+  ExifTool
+} = require("exiftool-vendored")
 const exiftool = new ExifTool()
 const filepath = 'problematic.jpg'
 
@@ -8,6 +10,16 @@ exiftool
   .read(filepath)
   .then(tags => {
     console.log('got tags : ', Object.keys(tags).join(', '))
+    console.log('reading went good')
+  })
+  .then(() => {
+    console.log('writing a new date')
+    return exiftool.write(filepath, {
+      AllDates: "2016-02-06T16:56:00"
+    })
+  })
+  .catch(err => {
+    console.log('error happend :', err)
   })
   .then(() => {
     console.log('closing exiftool instance')
